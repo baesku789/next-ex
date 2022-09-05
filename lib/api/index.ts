7069f0393cf63406ed1cc5c702ddc4f09ea97dd4
yechan <baesku789@gmail.com>
2022-09-05 16:56:33 +0900

    refactor: axios instance 생성

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,8 +1,25 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	baseURL: 'https://kosis.kr/openapi',
+	params: {
+		apiKey: 'MzJkNDQyM2RiYjUzMGQxOWYwOGQ4ODlkMmE1ZTczZDQ=',
+	},
+});
+
 export async function getKosisList() {
-	const { data, status } = await axios.get<Response>(
-		'https://kosis.kr/openapi/statisticsList.do?method=getList&apiKey=MzJkNDQyM2RiYjUzMGQxOWYwOGQ4ODlkMmE1ZTczZDQ=&vwCd=MT_ZTITLE&parentListId=J1&format=json&jsonVD=Y&version=v2_1'
+	const { data, status } = await instance.get<Response>(
+		'/statisticsList.do',
+		{
+			params: {
+				method: 'getList',
+				format: 'json',
+				jsonVD: 'Y',
+				vwCd: 'MT_ZTITLE',
+				parentListId: 'J1',
+				version: 'v2_1',
+			},
+		}
 	);
 
 	return data;
