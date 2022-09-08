42adbe9a2a1844e0c08a6e737a47dc08ca1f2bef
yechan <baesku789@gmail.com>
2022-09-08 13:00:08 +0900

    feat: 홈페이지 통계목록 링크 추가, header 아이콘 클릭시 홈페이지로 라우팅, recoil 추가

--- components/header.js
+++ components/header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+	const router = useRouter()
+
 	return (<div className={'pl-5 border-solid border-gray-50 border-b-2 h-30 flex justify-start items-center'}>
-		<Image src={'/images/home_black_24dp.svg'} width={24} height={24}/>
+		<Image onClick={() => router.push('/')} src={'/images/home_black_24dp.svg'} width={24} height={24}/>
 	</div>);
 };
 
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ErrorBoundary from '../components/Errorboundary';
 import Layout from '../components/layout';
+import { RecoilRoot } from 'recoil';
 
 // This default export is required in a new `pages/_app.jsx` file.
 export default function MyApp({ Component, pageProps }) {
@@ -26,9 +27,11 @@ export default function MyApp({ Component, pageProps }) {
 		<QueryClientProvider client={queryClient}>
 			<Hydrate state={pageProps.dehydratedState}>
 				<ErrorBoundary FallbackComponent={<div>Error</div>}>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
+					<RecoilRoot>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</RecoilRoot>
 				</ErrorBoundary>
 				<ReactQueryDevtools initialIsOpen={false}/>
 			</Hydrate>
