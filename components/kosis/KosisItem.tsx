639c9bf68f7d8bef8a1e0924a6acd0b44b8a6fb2
yechan <baesku789@gmail.com>
2022-09-15 17:24:38 +0900

    refactor: - KosisItem.tsx 디자인 수정

--- components/kosis/KosisItem.tsx
+++ components/kosis/KosisItem.tsx
@@ -8,9 +8,9 @@ interface KosisItemProps {
 
 export default function KosisItem({ href, title }: KosisItemProps) {
     return (
-        <li>
+        <li className={'border-black border-1 border-solid rounded-5 p-10'}>
             <Link href={href}>
-                <div className={'flex gap-8'}>
+                <div className={'flex gap-8 justify-between'}>
                     <h1>{title}</h1>
                     <Image src={'/images/east_black_24dp.svg'} width={24} height={24} />
                 </div>
--- pages/kosis.tsx
+++ pages/kosis.tsx
@@ -3,7 +3,7 @@ import KosisItem from '../components/kosis/KosisItem';
 export default function Kosis() {
     return (
         <div className={'p-10'}>
-            <ul>
+            <ul className={'flex flex-col gap-10'}>
                 <KosisItem href={'/economy'} title={'경제 일반 ㆍ 경기'} />
                 <KosisItem href={'/prices'} title={'물가'} />
             </ul>
