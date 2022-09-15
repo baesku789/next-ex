0a4ee5ad7f33cd681da62f6ea324a22e0d4a171e
yechan <baesku789@gmail.com>
2022-09-15 15:42:06 +0900

    refactor: 아이폰 100vh 위아래 바 제외한 값으로 설정

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,46 +1,53 @@
 import '../style/styles.css';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ErrorBoundary from '../components/Errorboundary';
 import Layout from '../components/layout';
 import { RecoilRoot } from 'recoil';
 import DebugObserver from '../recoil/DebugObserver';
-import Head from 'next/head'
+import Head from 'next/head';
 
 // This default export is required in a new `pages/_app.jsx` file.
 export default function MyApp({ Component, pageProps }) {
 
-	const [queryClient] = useState(
-		() =>
-			new QueryClient({
-				defaultOptions: {
-					queries: {
-						retry: false,
-						retryOnMount: false,
-						refetchOnMount: false,
-						refetchOnWindowFocus: false,
-					},
-				},
-			})
-	);
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        retry: false,
+                        retryOnMount: false,
+                        refetchOnMount: false,
+                        refetchOnWindowFocus: false
+                    }
+                }
+            })
+    );
 
-	return (
-		<QueryClientProvider client={queryClient}>
-			<Hydrate state={pageProps.dehydratedState}>
-				<ErrorBoundary FallbackComponent={<div>Error</div>}>
-					<RecoilRoot>
-						<DebugObserver/>
-						<Head>
-							<meta name={'viewport'} content={'user-scalable=no initial-scale=1 width=device-width'} />
-						</Head>
-						<Layout>
-							<Component {...pageProps} />
-						</Layout>
-					</RecoilRoot>
-				</ErrorBoundary>
-				<ReactQueryDevtools initialIsOpen={false}/>
-			</Hydrate>
-		</QueryClientProvider>
-	);
+    let vh = 0;
+
+    useEffect(() => {
+        vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, []);
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <ErrorBoundary FallbackComponent={<div>Error</div>}>
+                    <RecoilRoot>
+                        <DebugObserver />
+                        <Head>
+                            <meta name={'viewport'} content={'user-scalable=no initial-scale=1 width=device-width'} />
+                        </Head>
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </RecoilRoot>
+                </ErrorBoundary>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </Hydrate>
+        </QueryClientProvider>
+    );
 }
